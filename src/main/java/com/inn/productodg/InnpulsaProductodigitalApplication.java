190a2da31030f6f98ef5293779c5f3ed298196fe
harry.wong <harry.wong@pbm.com.co>
2023-01-29 20:47:42 -0500

    v0.5.1

--- src/main/java/com/inn/productodg/InnpulsaProductodigitalApplication.java
+++ src/main/java/com/inn/productodg/InnpulsaProductodigitalApplication.java
@@ -8,20 +8,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import com.inn.productodg.domain.Car;
-import com.inn.productodg.domain.CarRepositorio;
-import com.inn.productodg.domain.PropietRepositorio;
-import com.inn.productodg.domain.Propietario;
-
 
 
 @SpringBootApplication
 public class InnpulsaProductodigitalApplication {
-
-	@Autowired
-	private CarRepositorio repository;
-	@Autowired
-	private PropietRepositorio prepositorio;
 
 	
 	public static void main(String[] args) {
@@ -32,7 +22,7 @@ public class InnpulsaProductodigitalApplication {
 		// appl innpulsa version 0.1
 		SpringApplication.run(InnpulsaProductodigitalApplication.class, args);
 		
-		milog.info("Producto digital Innpulsa, version 0.1");
+		milog.info("Producto digital Innpulsa, version 0.5");
 	}
 	
 
@@ -40,14 +30,6 @@ public class InnpulsaProductodigitalApplication {
 	@Bean
 	CommandLineRunner runner() {
 		return args -> {
-			Propietario p1 = new Propietario("John" , "Johnson");
-			Propietario p2 = new Propietario("Mary" , "Robinson");
-			prepositorio.save(p1);
-			prepositorio.save(p2);
-
-			repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2017, 59000, p1));
-			repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2014, 29000, p1));
-			repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2018, 39000, p2));
 		};
 	}	
 
